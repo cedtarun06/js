0904a3c997eb18f61cc23b1a151827ee050546ef
Tarun Soni <TarunSoni@cedcoss.com>
2022-02-07 10:24:11 +0530

    Understanding Uninitialized Variables

--- src/index.js
+++ src/index.js
@@ -8,3 +8,13 @@ b = a;
 var a = 9;
 var myFirstName = "Tarun";
 var myLastName = "Soni";
+a =5;
+var b;
+b= 10;
+var c;
+c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
