5e0c188a9fa06ef78976ba445690d61090ef1096
Tarun Soni <TarunSoni@cedcoss.com>
2022-02-07 11:50:51 +0530

    Compound Assignment With Augmented Division

--- src/index.js
+++ src/index.js
@@ -44,3 +44,11 @@ const difference = 45 - 33;
 // Change this line
 console.log(FCC, fact);
 console.log(FCC, fact);
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /=12;
+b /=4;
+c /=11;
